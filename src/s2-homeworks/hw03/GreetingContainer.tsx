378509be56b0b0cf44cc485d99472dbe69ddac8e
Misha <keebirg@yandex.ru>
2023-10-25 16:46:13 +0700

    HW3_2week_debag1

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -59,7 +59,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     }
 
     const totalUsers = users.length // need to fix
-    const lastUserName = name // need to fix
+    const lastUserName = users.length ? users[users.length-1].name : undefined // need to fix
 
     return (
         <Greeting
