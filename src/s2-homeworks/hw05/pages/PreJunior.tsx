f2daaf7807fe8c792e53456fe3b27763523b4052
Misha <keebirg@yandex.ru>
2024-02-15 20:32:20 +0700

    HW5

--- src/s2-homeworks/hw05/pages/PreJunior.tsx
+++ src/s2-homeworks/hw05/pages/PreJunior.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import s from '../../../s1-main/App.module.css'
 import HW1 from '../../hw01/HW1'
 import HW2 from '../../hw02/HW2'
 import HW3 from '../../hw03/HW3'
@@ -8,10 +9,12 @@ function PreJunior() {
     return (
         <div id={'hw5-page-pre-junior'}>
             {/*pre junior page*/}
-            <HW1 />
-            <HW2 />
-            <HW3 />
-            <HW4 />
+            <div className={s.App}>
+                <HW1 />
+                <HW2 />
+                <HW3 />
+                <HW4 />
+            </div>
         </div>
     )
 }
