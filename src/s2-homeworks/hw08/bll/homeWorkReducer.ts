83dd52b5da04a702b92a7d7b7c42bd93478b2168
keebirg <keebirg@yandex.ru>
2024-05-26 19:00:52 +0700

    add hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,22 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            let newState = [...state];
+            if(action.payload=='up'){
+                newState.sort((a, b) => a.name.localeCompare(b.name))
+            }
+            if(action.payload=='down'){
+                newState.sort((a, b) => b.name.localeCompare(a.name))
+            }
+
+            return newState // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user=>user.age>=18)
         }
         default:
             return state
